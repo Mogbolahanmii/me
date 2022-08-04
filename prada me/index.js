48f56241e42be641cafa44df55c1a34cc6732e91
Mogbolahanmii <107737603+Mogbolahanmii@users.noreply.github.com>
2022-08-05 00:32:13 +0100

    git commit prada clone website

---
+++ prada me/index.js
@@ -0,0 +1,19 @@
+const womenPopUp = document.querySelector('.women-popup')
+const menPopUp = document.querySelector('.men-popup')
+const bagsPopUp = document.querySelector('.bags-popup')
+
+console.log(womenPopUp)
+console.log(menPopUp)
+console.log(bagsPopUp)
+
+function womencancel() {
+    womenPopUp.textContent = ''
+}
+
+function mencancel() {
+    menPopUp.textContent = ''
+}
+
+function bagscancel() {
+    bagsPopUp.textContent = ''
+}
