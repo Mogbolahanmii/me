343eab29f3f0c4d64a04bb2eb267077d6eb32dc1
Mogbolahanmii <107737603+Mogbolahanmii@users.noreply.github.com>
2022-08-06 01:22:00 +0100

    prada FW22 update

--- prada me/index.js
+++ prada me/index.js
@@ -1,10 +1,12 @@
 const womenPopUp = document.querySelector('.women-popup')
 const menPopUp = document.querySelector('.men-popup')
 const bagsPopUp = document.querySelector('.bags-popup')
+// const navLi = document.querySelector('.nav-li')
 
 console.log(womenPopUp)
 console.log(menPopUp)
 console.log(bagsPopUp)
+console.log(navLi)
 
 function womencancel() {
     womenPopUp.textContent = ''
