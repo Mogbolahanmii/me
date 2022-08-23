67c1ef3f218f4b2b4eb1a3a2175388574e60645a
Mogbolahanmii <107737603+Mogbolahanmii@users.noreply.github.com>
2022-08-23 23:46:36 +0100

    responsive prada me

--- prada me/index.js
+++ prada me/index.js
@@ -1,19 +1,22 @@
 const womenPopUp = document.querySelector('.women-popup')
 const menPopUp = document.querySelector('.men-popup')
 const bagsPopUp = document.querySelector('.bags-popup')
+const menCancel = document.getElementById('mencancel')
+const womenCancel = document.getElementById('womencancel')
+const bagsCancel = document.getElementById('bagscancel')
 
-console.log(womenPopUp)
-console.log(menPopUp)
-console.log(bagsPopUp)
-
-function womencancel() {
+womenCancel.addEventListener('click', womencancel)
+function womencancel(){
     womenPopUp.textContent = ''
+    // womenPopUp.style.display = 'none'
 }
 
-function mencancel() {
+menCancel.addEventListener('click', mencancel)
+function mencancel(){
     menPopUp.textContent = ''
 }
 
-function bagscancel() {
+bagsCancel.addEventListener('click', bagscancel)
+function bagscancel(){
     bagsPopUp.textContent = ''
 }
