61448bf5a471ecf8cbe1101d82eb47273a49b918
Mogbolahanmii <107737603+Mogbolahanmii@users.noreply.github.com>
2022-10-15 23:13:24 +0100

    searchbar icon update

--- prada me/index.js
+++ prada me/index.js
@@ -1,22 +1,37 @@
-const womenPopUp = document.querySelector('.women-popup')
-const menPopUp = document.querySelector('.men-popup')
-const bagsPopUp = document.querySelector('.bags-popup')
-const menCancel = document.getElementById('mencancel')
 const womenCancel = document.getElementById('womencancel')
+const menCancel = document.getElementById('mencancel')
 const bagsCancel = document.getElementById('bagscancel')
+const search = document.getElementById('search')
 
 womenCancel.addEventListener('click', womencancel)
 function womencancel(){
-    womenPopUp.textContent = ''
-    // womenPopUp.style.display = 'none'
+    const womenPopUp = document.querySelector('.women-popup')
+    womenPopUp.style.display = 'none'
+    // womenPopUp.textContent = ''
 }
 
 menCancel.addEventListener('click', mencancel)
 function mencancel(){
-    menPopUp.textContent = ''
+    const menPopUp = document.querySelector('.men-popup')
+    menPopUp.style.display = 'none'
+    // menPopUp.textContent = ''
+    // menPopUp.style.visibility = 'hidden'
 }
 
 bagsCancel.addEventListener('click', bagscancel)
 function bagscancel(){
-    bagsPopUp.textContent = ''
+    const bagsPopUp = document.querySelector('.bags-popup')
+    bagsPopUp.style.display = 'none'
+    // bagsPopUp.textContent = ''
 }
+
+search.addEventListener('mouseover', searchSite)
+function searchSite() {
+    search.innerHTML = ''
+    const searchSite = document.getElementById('searchsite')
+    searchSite.style = 'display: block; position: fixed; right: 0; margin: 0.7rem;'
+}
+
+// function renew() {
+//     menPopUp.style.visibility = 'visible'
+// }
